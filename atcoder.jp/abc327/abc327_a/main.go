a5ab58d321682d4eef57e557abad86f2d72b564b
newbie1111 <harehare753@gmail.com>
2023-12-27 22:13:27 +0900

    解答コードを追加

--- atcoder.jp/abc327/abc327_a/main.go
+++ atcoder.jp/abc327/abc327_a/main.go
@@ -8,7 +8,6 @@ import (
 	"io"
 	"log"
 	"os"
-	"reflect"
 	"strconv"
 	"strings"
 )
@@ -19,115 +18,27 @@ problem solver
 
 func main() {
 	var (
-		n, m int
-		p    []string
-		s    []string
+		N int
+		S string
 	)
 
-	input.Scan()
-	line, _ := ParseSignedInts[int](input.Text(), " ")
-	n, m = line[0], line[1]
+	fmt.Scan(&N, &S)
 
-	for input.Scan() {
-		line := strings.Split(input.Text(), " ")
-		p = append(p, line[0])
-		s = append(s, line[1])
-	}
-
-	fmt.Println(solve3(n, m, p, s))
+	ans := solve(N, S)
+	output.Println(ans)
 }
 
-func solve(n, m int, p []string, s []string) (any, any) {
-	var (
-		mp          = make(map[string]string, 0)
-		ac, penalty int
-		AC          = "AC"
-	)
+func solve(N int,
+	S string,
+) any {
+	debug.Println(N, S)
 
-	debug.Println(n, m, p, s)
-
-	for i := 0; i < m; i++ {
-		debug.Println(mp, ac, penalty)
-
-		if mp[p[i]] != AC {
-			if s[i] == AC {
-				mp[p[i]] = AC
-				ac++
-			} else {
-				penalty++
-			}
-		}
-
-		debug.Println(mp, ac, penalty)
+	if strings.Contains(S, "ab") || strings.Contains(S, "ba") {
+		return Yes
+	} else {
+		return No
 	}
 
-	return ac, penalty
-}
-
-func solve2(n, m int, p []string, s []string) (any, any) {
-	var (
-		status  = make(map[string]string)
-		penalty = make(map[string]int)
-		ac, wa  int
-		AC      = "AC"
-	)
-
-	debug.Println(n, m, p, s)
-	debug.Println(status, penalty)
-
-	for i := 0; i < m; i++ {
-
-		debug.Println(p[i], s[i], status, penalty, ac, wa)
-
-		if s[i] == AC {
-			if status[p[i]] != AC {
-				status[p[i]] = AC
-				ac++
-				wa += penalty[p[i]]
-			}
-		} else {
-			if status[p[i]] != AC {
-				penalty[p[i]]++
-			}
-		}
-
-		debug.Println(status, penalty, ac, wa)
-	}
-
-	return ac, wa
-}
-
-func solve3(n, m int, P []string, S []string) (any, any) {
-
-	var (
-		acList = make([]int, n)
-		waList = make([]int, n)
-	)
-
-	for i, s := range S {
-		p, err := strconv.Atoi(P[i])
-		if err != nil {
-			panic(err)
-		}
-		p--
-
-		if s == "AC" {
-			acList[p] = 1
-		} else {
-			if acList[p] == 0 {
-				waList[p]++
-			}
-		}
-	}
-
-	// debug.Println(acList, waList)
-	waSum := 0
-	for i, wa := range waList {
-		waSum += acList[i] * wa
-	}
-
-	// return Sum[int](acList...), Sum[int](waList...)
-	return Sum[int](acList...), waSum
 }
 
 /*
@@ -135,8 +46,11 @@ initialize common variables
 */
 
 var (
-	input             = bufio.NewScanner(os.Stdin)
-	output, debug     = log.New(os.Stdout, "", 0), log.New(io.Discard, "DEBUG : ", log.Lshortfile)
+	input         = bufio.NewScanner(os.Stdin)
+	output, debug = log.New(os.Stdout, "", 0), log.New(io.Discard, "DEBUG : ", log.Lshortfile)
+)
+
+const (
 	YES, Yes, yes     = "YES", "Yes", "yes"
 	NO, No, no        = "NO", "No", "no"
 	Even              = "Even"
@@ -175,10 +89,11 @@ type Zahl interface {
 }
 
 type SignedInteger interface {
-	int | int32 | int64
+	int | int8 | int16 | int32 | int64
 }
+
 type UnSignedInteger interface {
-	uint | uint32 | uint64
+	uint | uint8 | uint16 | uint32 | uint64
 }
 
 type Float interface {
@@ -186,19 +101,12 @@ type Float interface {
 }
 
 /*
-Input / Output
-*/
-
-func dump(variable ...interface{}) {
-	for _, v := range variable {
-		debug.Printf("%v : %v\n", reflect.TypeOf(v), v)
-	}
-}
-
-/*
 Data Structures
 - Set
 - Queue
+- Circular Queue
+- Vector
+- Matrix
 */
 
 type Set[T comparable] map[T]struct{}
@@ -277,10 +185,227 @@ func (sq *SliceQueue[T]) Rotate(n int) {
 	}
 }
 
+type CircularQueue[Any any] struct {
+	head, tail int
+	cq         []Any
+}
+
+func NewCircularQueue[Any any]() CircularQueue[Any] {
+	newCircularQueue := *new(CircularQueue[Any])
+	newCircularQueue.InIt()
+	return newCircularQueue
+}
+
+func (cq *CircularQueue[Any]) InIt() {
+	cq.head, cq.tail = 0, 0
+	cq.cq = make([]Any, 1)
+}
+
+func (cq *CircularQueue[Any]) nextIndex(index int) int {
+	return (index + 1) % len(cq.cq)
+
+}
+
+func (cq *CircularQueue[Any]) previousIndex(index int) int {
+	return (len(cq.cq) + index - 1) % len(cq.cq)
+
+}
+
+func (cq *CircularQueue[Any]) Full() bool {
+	return cq.head == cq.nextIndex(cq.tail)
+}
+
+func (cq *CircularQueue[Any]) Empty() bool {
+	return cq.head == cq.tail
+}
+
+func (cq *CircularQueue[Any]) Length() int {
+	return Abs(cq.tail - cq.head)
+}
+
+func (cq *CircularQueue[Any]) toSlice() []Any {
+	res := make([]Any, 0, cq.Length())
+	for i := cq.head; i != cq.tail; i = cq.nextIndex(i) {
+		res = append(res, cq.cq[i])
+	}
+	return res
+}
+
+func (cq *CircularQueue[Any]) PushLeft(x Any) {
+	if cq.Full() {
+		prevCQ := cq.toSlice()
+		newCQ := make([]Any, len(cq.cq)<<1)
+
+		newCQ[0] = x
+		copy(newCQ[1:], prevCQ)
+
+		cq.head, cq.tail = 0, len(prevCQ)+1
+		cq.cq = newCQ
+	} else {
+		cq.head = cq.previousIndex(cq.head)
+		cq.cq[cq.head] = x
+	}
+}
+
+func (cq *CircularQueue[Any]) PushRight(x Any) {
+	if cq.Full() {
+		prevCQ := cq.toSlice()
+		newCQ := make([]Any, len(cq.cq)<<1)
+
+		copy(newCQ, prevCQ)
+		newCQ[len(prevCQ)] = x
+
+		cq.head, cq.tail = 0, len(prevCQ)+1
+		cq.cq = newCQ
+	} else {
+		cq.cq[cq.tail] = x
+		cq.tail = cq.nextIndex(cq.tail)
+	}
+}
+
+func (cq *CircularQueue[Any]) PopRight() Any {
+	if cq.Empty() {
+		return *new(Any)
+	} else {
+		cq.tail = cq.previousIndex(cq.tail)
+		res := cq.cq[cq.tail]
+		return res
+	}
+}
+
+func (cq *CircularQueue[Any]) At(index int) Any {
+	return cq.cq[(cq.head+index)%len(cq.cq)]
+}
+
+func (cq *CircularQueue[Any]) SetAt(index int, value Any) {
+	cq.cq[(cq.head+index)%len(cq.cq)] = value
+}
+
+type Vector[R Real] []R
+
+func NewVector[R Real](v ...R) Vector[R] {
+	newVec := new(Vector[R]).InIt(len(v))
+	copy(newVec, v)
+	return newVec
+}
+
+func (v *Vector[R]) InIt(dim int) Vector[R] {
+	return make(Vector[R], dim)
+}
+
+func (v *Vector[R]) Reshape(dim int) Vector[R] {
+	nv := new(Vector[R]).InIt(dim)
+	copy(nv, *v)
+	return nv
+}
+
+func (v *Vector[R]) Dimension() int {
+	return len(*v)
+}
+
+func (v *Vector[R]) ScalarAdd(n R) {
+	for i := 0; i < v.Dimension(); i++ {
+		(*v)[i] += n
+	}
+}
+
+func (v *Vector[R]) ScalarSubtract(n R) {
+	for i := 0; i < v.Dimension(); i++ {
+		(*v)[i] -= n
+	}
+}
+
+func (v *Vector[R]) Difference(diff Vector[R]) Vector[R] {
+	nv := NewVector[R](*v...)
+
+	if v.Dimension() != diff.Dimension() {
+		nv.Reshape(Max(v.Dimension(), diff.Dimension()))
+	}
+
+	for index, value := range diff {
+		nv[index] -= value
+	}
+
+	return nv
+}
+
+func (v *Vector[R]) ScalarMultipul(n R) {
+	for i := 0; i < v.Dimension(); i++ {
+		(*v)[i] *= n
+	}
+}
+
+func (v *Vector[R]) ScalarDivide(n R) {
+	for i := 0; i < v.Dimension(); i++ {
+		(*v)[i] /= n
+	}
+}
+
+type Matrix[R Real] []Vector[R]
+
+func NewMatrix[R Real](rows ...[]R) Matrix[R] {
+	maxDim := 0
+	for _, row := range rows {
+		maxDim = Max(maxDim, len(row))
+	}
+
+	newMat := new(Matrix[R]).InIt(len(rows), maxDim)
+	for i, row := range rows {
+		copy(newMat[i], row)
+	}
+
+	return newMat
+}
+
+func (mat *Matrix[R]) InIt(row, col int) Matrix[R] {
+	newMat := make(Matrix[R], 0, row*col)
+	for i := 0; i < row; i++ {
+		rowVector := NewVector[R]()
+		rowVector.Reshape(col)
+		newMat = append(newMat, rowVector)
+	}
+	return newMat
+}
+
+func (mat *Matrix[R]) Size() int {
+	if len(*mat) == 0 {
+		return 0
+	} else {
+		return len(*mat) * len((*mat)[0])
+	}
+}
+
+func (mat *Matrix[R]) Dimension() (int, int) {
+	if mat.Size() == 0 {
+		return 0, 0
+	} else {
+		return len(*mat), len((*mat)[0])
+	}
+}
+
+func (mat *Matrix[R]) RowAt(index int) Vector[R] {
+	row := (*mat)[index][:]
+	return NewVector(row...)
+}
+
+func (mat *Matrix[R]) ColAt(index int) Vector[R] {
+	var (
+		r, _ = mat.Dimension()
+		col  = new(Vector[R]).InIt(r)
+	)
+
+	for i, vec := range *mat {
+		col[i] = vec[index]
+	}
+
+	return col
+}
+
 /*
 Character / String
 - String Reversal
 - string to integer slice
+- integer slice to string
 */
 
 func ReverseString(s string) string {
@@ -310,6 +435,18 @@ func ParseSignedInts[SI SignedInteger](line, sep string) ([]SI, error) {
 	return res, nil
 }
 
+func FormatSignedInts[SI SignedInteger](s []SI, base int, sep string) string {
+	var (
+		res []string
+	)
+
+	for _, v := range s {
+		res = append(res, strconv.FormatInt(int64(v), base))
+	}
+
+	return strings.Join(res, sep)
+}
+
 func ParseUnSignedInts[USI UnSignedInteger](line, sep string) ([]USI, error) {
 	var (
 		splitWords = strings.Split(line, sep)
@@ -331,6 +468,7 @@ func ParseUnSignedInts[USI UnSignedInteger](line, sep string) ([]USI, error) {
 Convert value
 - Absolute value
 - Power
+- Ceil
 */
 
 // return absolute integer
@@ -357,9 +495,15 @@ func PowInteger[Z Zahl](a, n Z) Z {
 	return res
 }
 
+func Ceil[Z Zahl](a, b Z) Z {
+	return (a + b - 1) / b
+}
+
 /*
 Number theoretic algorithm
 - total
+- factorial
+- product
 - Enumerate all divisors
 - check prime number
 - Cumulative Sum
@@ -379,6 +523,39 @@ func Sum[R Real](vars ...R) R {
 	}
 
 	return sum
+}
+
+func Factorial[Z Zahl](n Z) Z {
+	res := make([]Z, 0)
+	for i := Z(1); i < n; i++ {
+		res = append(res, i)
+	}
+	return Product[Z](res...)
+}
+
+func Product[R Real](vars ...R) R {
+	if len(vars) == 0 {
+		return 0
+	} else {
+		product := R(1)
+		for _, v := range vars {
+			product *= v
+		}
+		return product
+	}
+}
+
+func ProductWithMod[Z Zahl](mod Z, vars ...Z) Z {
+	if len(vars) == 0 {
+		return 0
+	} else {
+		product := Z(1)
+		for _, v := range vars {
+			product *= v
+			product %= mod
+		}
+		return product
+	}
 }
 
 func Divisors[Z Zahl](n Z) []Z {
@@ -404,7 +581,7 @@ func Divisors[Z Zahl](n Z) []Z {
 	return div
 }
 
-func isPrime[Z Zahl](n Z) bool {
+func IsPrime[Z Zahl](n Z) bool {
 	switch {
 	case n < 2:
 		return false
@@ -438,7 +615,7 @@ func CumulativeSum[R Real](vars []R) []R {
 
 func ArithmeticProgressionsSum[R Real, Z Zahl](a0, d R, n Z) (sum R, err error) {
 	if n < 0 {
-		sum, err = 0, errors.New("number of terms must not be negative.")
+		sum, err = 0, errors.New("number of terms must not be negative")
 	} else {
 		nReal := R(n)
 		sum, err = (a0*nReal)+nReal*(nReal-1)/2*d, nil
@@ -449,7 +626,7 @@ func ArithmeticProgressionsSum[R Real, Z Zahl](a0, d R, n Z) (sum R, err error) 
 
 func GeometicProgressionsSum[Z Zahl](a0, r, n Z) (sum Z, err error) {
 	if n < 0 {
-		sum, err = 0, errors.New("number of terms must not be negative.")
+		sum, err = 0, errors.New("number of terms must not be negative")
 	} else {
 		sum, err = a0*(PowInteger(r, n)-1)/(r-1), nil
 	}
@@ -501,6 +678,7 @@ func LeastCommonMultipulIntegers[I Zahl](vars ...I) (lcm I, err error) {
 Comparison of Values
 - larger / smaller
 - largest / smallest
+- check all values
 */
 
 func Max[R Real](x, y R) R {
@@ -555,14 +733,25 @@ func MinValues[R Real](vars ...R) (R, error) {
 	return res, err
 }
 
+func CheckAllValues[T comparable](fn func(v T) bool, values ...T) bool {
+	res := true
+
+	for _, v := range values {
+		res = res && fn(v)
+	}
+
+	return res
+}
+
 /*
 Search Algorithm
 - Binary Search
+- Measuring Worm Algorithm
 */
 
-func BinarySearch[Z Zahl](negative, positive, dist Z,
-	IsContinue func(negative, positive, dist Z) bool,
-	IsPositive func(mid Z) bool) (Z, Z) {
+func BinarySearch[R Real](negative, positive, dist R,
+	IsContinue func(negative, positive, dist R) bool,
+	IsPositive func(mid R) bool) (R, R) {
 
 	for IsContinue(negative, positive, dist) {
 		mid := negative + Abs(positive-negative)/2
